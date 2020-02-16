7e20e0017f063aeaabe257948e18901d7c013980
anasvakyathodi <44634141+anasvakyathodi@users.noreply.github.com>
2020-02-16 15:36:30 +0530

    lab update

--- teacher/lab.php
+++ teacher/lab.php
@@ -7,52 +7,65 @@ require $_SERVER['DOCUMENT_ROOT'].'/db_connect.php';
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="/teacher/assets/css/style.css">
-    <title>Dashboard | Teacher</title>
+    <link rel="stylesheet" href="../teacher/assets/css/style.css">
+    <title>Lab Page | Teacher</title>
   </head>
   <body>
-    <div class="container py-4">
-      <div class="row py-4">
-        <div class="col-3 hello"><h3>Hello, $user!</h3></div>
-        <div class="col-9 lab-title"><h3>Network Programming Lab</h3></div>
+    <nav class="navbar navbar-dark navbar-expand-lg customnav">
+    <div class="container">
+      <a class="navbar-brand" href="">
+        Hehe
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="navbar-nav ml-auto desktop_menu">
+          <a class="nav-item nav-link" href=""><i class="material-icons">account_circle</i></a>
+        </div>
+        <div class="navbar-nav ml-auto phone_menu">
+          <a class="nav-item nav-link" href=""><b>Dashboard</b></a>
+          <a class="nav-item nav-link" href=""><b>Profile</b></a>
+          <a class="nav-item nav-link" href="/logout.php"><b>Logout</b></a>
+        </div>
       </div>
+    </div>
+    </nav>
+
+    <div class="container py-4 maincontent">
       <div class="row py-4">
         <div class="col-3 sidenav pr-5">
           <a href="" class="text-decoration-none navbutton"><div>Dashboard</div></a>
           <a href="" class="text-decoration-none navlink">Logout</a>
         </div>
-        <div class="col customcontent">
-          <div class="row">
-            <div class="col">
-            <form class="form-inline">
-                  <div class="form-group">
-                    <input type="password" class="form-control" id="experiment" placeholder="Experiment">
+              <div class="col sub-page-title">
+                  <div class="form-group srch">
+                    <input class="form-control aim" type="text" placeholder="Experiment Aim">
+                    <button type="submit" class="btn btn-success">Add New Experiment</button>
                   </div>
-                  <button type="submit" class="btn btn-primary">Add new experiment</button>
-            </form>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col"><a href="" class="text-decoration-none labbutton"><div>Network Programming Lab<p>S2</p></div></a></div>
-            <div class="col"><a href="" class="text-decoration-none labbutton"><div>System Software Lab<p>S4</p></div></a></div>
-          </div>
-          <div class="row">
-            <div class="col"><a href="" class="text-decoration-none labbutton"><div>Network Programming Lab<p>S6</p></div></a></div>
-            <div class="col"><a href="" class="text-decoration-none labbutton"><div>System Software Lab<p>S8</p></div></a></div>
-          </div>
-          <div class="row py-4">
-            <div class="col newlablink ">
-            <a href="">+New lab</a>
-            </div>
-            <div class="col prevlabslink ">
-            <a href="">View past labs</a>
-            </div>
-          </div>
+                <h5>Experiments</h5>
+                <div class="list-group">
+                  <a href="#" class="list-group-item list-group-item-action list-group-item-success lst">02/02/2020 Implementation of stacks</a>
+                  <a href="#" class="list-group-item list-group-item-action list-group-item-success lst">04/02/2020 Implementation of Queues in C</a>
+                  <a href="#" class="list-group-item list-group-item-action list-group-item-success lst">02/02/2020 Implementation of Binary Search Trees using C</a>
+                  <a href="#" class="list-group-item list-group-item-action list-group-item-success lst">02/02/2020 Implementation of hashing tables using unordered map in C</a>
+                </div>
+                <div class="form-group">
+                  <button type="button" class="btn btn-danger pend">Pending Students</button>
+                </div>
+              </div>
         </div>
+          </div>
+    <footer class="myfooter">
+      <div class="container">
+         <span class="text-muted">:D</span>
       </div>
-    </div>
-
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
