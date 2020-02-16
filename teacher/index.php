2c366091eae8e47a6749659139310e004ea77f23
anasvakyathodi <44634141+anasvakyathodi@users.noreply.github.com>
2020-02-16 15:35:13 +0530

    index update

--- teacher/index.php
+++ teacher/index.php
@@ -7,41 +7,72 @@ require $_SERVER['DOCUMENT_ROOT'].'/db_connect.php';
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="/teacher/assets/css/style.css">
-    <title>Dashboard | Teacher</title>
+    <link rel="stylesheet" href="../teacher/assets/css/style.css">
+    <title>Experiment | Teacher</title>
   </head>
   <body>
-    <div class="container py-4">
-      <div class="row py-4">
-        <div class="col hello"><h3>Hello, $user!</h3></div>
+    <nav class="navbar navbar-dark navbar-expand-lg customnav">
+    <div class="container">
+      <a class="navbar-brand" href="">
+        Hehe
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <div class="navbar-nav ml-auto desktop_menu">
+          <a class="nav-item nav-link" href=""><i class="material-icons">account_circle</i></a>
+        </div>
+        <div class="navbar-nav ml-auto phone_menu">
+          <a class="nav-item nav-link" href=""><b>Dashboard</b></a>
+          <a class="nav-item nav-link" href=""><b>Profile</b></a>
+          <a class="nav-item nav-link" href="/logout.php"><b>Logout</b></a>
+        </div>
       </div>
+    </div>
+    </nav>
+
+    <div class="container py-4 maincontent">
       <div class="row py-4">
         <div class="col-3 sidenav pr-5">
           <a href="" class="text-decoration-none navbutton"><div>Dashboard</div></a>
           <a href="" class="text-decoration-none navlink">Logout</a>
         </div>
         <div class="col customcontent">
-          <div class="row">
-            <div class="col"><a href="" class="text-decoration-none labbutton"><div>Network Programming Lab<p>S2</p></div></a></div>
-            <div class="col"><a href="" class="text-decoration-none labbutton"><div>System Software Lab<p>S4</p></div></a></div>
+          <h3>2. Implement Queue in C</h3>
+          <div class="text-right">
+          <h5>04/02/2020</h5>
           </div>
-          <div class="row">
-            <div class="col"><a href="" class="text-decoration-none labbutton"><div>Network Programming Lab<p>S6</p></div></a></div>
-            <div class="col"><a href="" class="text-decoration-none labbutton"><div>System Software Lab<p>S8</p></div></a></div>
+          <div class="tocheck">
+          <label for="">To Check</label>
+          <a href="" type="button" class="btn btn-outline-dark">60. Aswathy</a>
+          <a href="" type="button" class="btn btn-outline-dark">14. Anaswara</a>
+          <a href="" type="button" class="btn btn-outline-dark">10. Amal</a>
           </div>
-          <div class="row py-4">
-            <div class="col newlablink ">
-            <a href="">+New lab</a>
-            </div>
-            <div class="col prevlabslink ">
-            <a href="">View past labs</a>
-            </div>
+          <div class="notsubmitted">
+          <label for="">Not Submitted</label>
+          <a href="" type="button" class="btn btn-outline-dark">12. Anand</a>
+          <a href="" type="button" class="btn btn-outline-dark">66. Sreedev</a>
+          </div>
+          <div class="completed">
+          <label for="">Completed</label>
+          <a href="" type="button" class="btn btn-outline-dark">65. Rentu</a>
+          <a href="" type="button" class="btn btn-outline-dark">64. Suchithra</a>
+          <a href="" type="button" class="btn btn-outline-dark">11. Anamika</a>
+          </div>
           </div>
         </div>
       </div>
-    </div>
-
+    <footer class="myfooter">
+      <div class="container">
+         <span class="text-muted">:D</span>
+      </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
